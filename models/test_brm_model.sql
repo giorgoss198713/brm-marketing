9d06e4e18a7fd1462be028844eee9d1078700b03
george.karageorgiou@gmail.com <george.karageorgiou@gmail.com>
2024-02-16 09:38:33 +0000

    modifying the case when on the test brm model

--- models/test_brm_model.sql
+++ models/test_brm_model.sql
@@ -1,4 +1,4 @@
-select id, created_date, dialer_id, (case when dialer_id=3 then 'CB10' end) as dialer_name 
+select id, created_date, dialer_id, (case when dialer_id=3 then 'CB10' when dialer_id=37 then 'C2' end) as dialer_name 
 from public_brm.marketing_leads
 where
 created_date>'2024-02-01'
