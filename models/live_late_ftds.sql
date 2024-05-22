537746d41f5dbf59e6dd0e85563c2f59aef32188
george.karageorgiou@gmail.com <george.karageorgiou@gmail.com>
2024-05-22 13:47:10 +0000

    correcting previous change for live late ftds table

--- models/live_late_ftds.sql
+++ models/live_late_ftds.sql
@@ -4,7 +4,8 @@ WHERE cast(ftd_date as date)=cast(created_date as date)),
 ftd_cte AS (SELECT id, 1 AS ftd_count from public_brm.marketing_leads_v2_dbt
 WHERE ftd is true)
 select to_char(ftd_date, 'YYYY (' ||
-to_char(ftd_date,'MM') || ') Mon') as month_year_ftd, dialer_name, country, 
+to_char(ftd_date,'MM') || ') Mon') as month_year_ftd, dialer_name, dialer_language,
+country, 
 coalesce(sum(live_ftd_count),0) as live_ftds,
 (coalesce(sum(ftd.ftd_count),0)-coalesce(sum(live_ftd_count),0)) as late_ftds,
 coalesce(sum(ftd.ftd_count),0) as total_ftds
@@ -14,4 +15,4 @@ left join ftd_cte ftd ON ftd.id=mlt2.id
 where
 ftd_date IS NOT NULL
 GROUP BY  to_char(ftd_date, 'YYYY (' ||
-to_char(ftd_date,'MM') || ') Mon'), country, dialer_name
+to_char(ftd_date,'MM') || ') Mon'), country, dialer_name, dialer_language
