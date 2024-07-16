34da7c3cdcc2b3d638bb4e04751855aa7a15fd46
george.karageorgiou@gmail.com <george.karageorgiou@gmail.com>
2024-07-16 14:18:27 +0000

    correcting ITL to BR2 in one point in independent expenses

--- models/independent_expenses_dbt.sql
+++ models/independent_expenses_dbt.sql
@@ -16,7 +16,7 @@ SELECT DISTINCT
         WHEN cet.dialer = 37 AND cm.dialer_campaign_id = 539 THEN 'TR/AZ'
 		WHEN cet.dialer = 37 AND cm.dialer_campaign_id <> 539 THEN 'RU'
         WHEN cet.dialer = 5 AND cm.dialer_campaign_id = 169 THEN 'ITL'
-        WHEN cet.dialer = 5 AND cm.dialer_campaign_id <> 169 THEN 'ITL'
+        WHEN cet.dialer = 5 AND cm.dialer_campaign_id <> 169 THEN 'BR2'
         ELSE dl.dialer_language 
     END as dialer_language,
     cm.dialer_campaign_id,
